67aabaf18da752df4e023fef582757b5818c76ee
Alecs <erick.borges@outlook.es>
2024-04-05 12:46:07 -0600

    fix: Multilanguage

---
+++ src/components/Language.js
@@ -0,0 +1,41 @@
+export function esp() {
+    //Login
+    const hello = '¡Hey! Bienvenido a';
+    const name = 'Mike BarberStudio';
+    const mailInput = 'Ingrese su email';
+    const passInput = 'Ingrese su contraseña';
+    const loginButton = 'Log in';
+    const createAccount = 'Crear una cuenta';
+    const forgotPassword = '¿Olvidaste tu contraseña?';
+
+    return({
+        hello,
+        name,
+        mailInput,
+        passInput,
+        loginButton,
+        createAccount,
+        forgotPassword
+    })
+};
+
+export function eng() {
+    //Login
+    const hello = 'Hey! Welcome to';
+    const name = 'Mike BarberStudio';
+    const mailInput = 'email';
+    const passInput = 'password';
+    const loginButton = 'Log in';
+    const createAccount = 'Create an Account';
+    const forgotPassword = 'Forgot your password?';
+    
+    return({
+        hello,
+        name,
+        mailInput,
+        passInput,
+        loginButton,
+        createAccount,
+        forgotPassword
+    })
+};
